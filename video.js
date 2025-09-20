bd14dcb130c2c1d3671831c0e9733980dde6edb9
NoHopeInLife <phambaoduylt@gmail.com>
2025-09-20 10:44:21 +0700

    them video

---
+++ video.js
@@ -0,0 +1,22 @@
+const projectData = [
+    {
+        title: "Drag System",
+        videoSrc: "drag_system.mp4",
+        poster: "poster_drag.jpg"
+    },
+    {
+        title: "Steal a Plane",
+        videoSrc: "Steal_a_plane.mp4",
+        poster: "poster_steal.jpg"
+    },
+    {
+        title: "Clicker Simulator System",
+        videoSrc: "click.mp4",
+        poster: "poster_click.jpg"
+    },
+    {
+        title: "Grow a Garden System",
+        videoSrc: "GAG.mp4",
+        poster: "poster_gag.jpg"
+    },
+];
